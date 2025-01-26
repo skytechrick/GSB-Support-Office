8d3b2bdff163235d056d6051678a63f72bc9174d
= <ricksarkar2005@gmail.com>
2025-01-27 02:00:30 +0530

    Internal server log add done.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-
+import { errorLog } from "./utils/logger.js";
 import auth from './routes/authRoute.js';
 import supportOffice from './routes/supportOfficeRoute.js';
 
@@ -20,6 +20,20 @@ app.use('/api/support-office', supportOffice);
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello World!' });
+});
+app.use( async (err, req, res, next) => {
+
+    if(req.isApi){
+        await errorLog(err);
+        return res.status(500).send({
+            message: "Internal server error",
+            error: process.env.NODE_ENV === 'development' ? err.message : undefined,
+        });
+    };
+
+    await errorLog(err);
+    // return res.status(500).sendFile('./public/error.html');
+    return res.status(500).send('500 error, Internal server error');
 });
 
 app.listen(process.env.PORT, () => {
